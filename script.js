2b12279e38d4fd960d51708b53301c4e2349a255
unknown <pablomcortes1990@gmail.com>
2022-10-10 18:32:35 -0300

    agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputsValues)
+
+function sumarInputsValues(event){
+    console.log({event})
+    event.preventDefault()
+    const sumaInputs = (parseInt(input1.value) + parseInt(input2.value));
+    pResult.innerText = "Resultado = " + sumaInputs;
+
+}
+
+
+
